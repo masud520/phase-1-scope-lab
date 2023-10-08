b7d9dd9cb2dc34b010548b203c3c9bc317b40138
masud520 <masud.abdi@student.moringaschool.com>
2023-10-08 13:07:52 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+
+// Declare a variable in global scope called customerName using the var keyword.
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'Masud';
+
+function upperCaseCustomerName(){
+    if(typeof customerName === 'string'){
+        customerName= customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = 'Zobegah'
+}
+
+
